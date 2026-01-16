53e2c33771f872e845d8cd76b0055116959bfc34
Tai Nguyen Van <buiblue123@gmail.com>
2026-01-16 16:03:06 +0700

    init project

---
+++ Controllers/PingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelTechApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Pong");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # traveltech-api
-# traveltech-api
