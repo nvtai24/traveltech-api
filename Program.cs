b38f74054b8eb66f50fe31eb74fb54df411f86e7
Tai Nguyen Van <buiblue123@gmail.com>
2026-03-10 21:21:32 +0700

    feat: enable public swagger

--- Program.cs
+++ Program.cs
@@ -24,11 +24,11 @@ namespace TravelTechApi
             // Configure the HTTP request pipeline
             app.UseExceptionHandler();
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            // if (app.Environment.IsDevelopment())
+            // {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            // }
 
             app.UseHttpsRedirection();
             app.UseCors("DefaultCorsPolicy");
