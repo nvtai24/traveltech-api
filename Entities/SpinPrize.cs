19bd8ab12be52e15fc52e09a65ac4a8fe67aa641
Tai Nguyen Van <buiblue123@gmail.com>
2026-03-10 22:44:52 +0700

    fix: utc datetime postgres in SpinPrize

--- Entities/SpinPrize.cs
+++ Entities/SpinPrize.cs
@@ -11,7 +11,7 @@ namespace TravelTechApi.Entities
         // public int Quantity { get; set; }
         // public int RemainingQuantity { get; set; }
         public bool IsActive { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
     }
 }
